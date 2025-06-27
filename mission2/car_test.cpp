38f61c8b498198299e36b2925815902dc2963ebe
AdMelioraDev <sj611.dev@gmail.com>
2025-06-27 17:25:04 +0900

    중간 결과 89

--- mission2/car_test.cpp
+++ mission2/car_test.cpp
@@ -25,6 +25,18 @@ TEST_F(CarTest, getNameForSedan)
     EXPECT_EQ(car->getName(), "Sedan");
 }
 
+TEST_F(CarTest, isEngineBrokenForSedanFalse)
+{
+    ICar* car = factory.getCar(SEDAN);
+    EngineMock engine_mock;
+
+    car->addEngine(&engine_mock);
+
+    EXPECT_CALL(engine_mock, getType).Times(1).WillOnce(::testing::Return(GM));
+
+    EXPECT_EQ(car->isEngineBroken(), false);
+}
+
 TEST_F(CarTest, isValidCheckForSedanReturnTrue)
 {
     ICar* car = factory.getCar(SEDAN);
@@ -101,6 +113,18 @@ TEST_F(CarTest, getNameForSuv)
     EXPECT_EQ(car->getName(), "SUV");
 }
 
+TEST_F(CarTest, isEngineBrokenForSuvrue)
+{
+    ICar* car = factory.getCar(SUV);
+    EngineMock engine_mock;
+
+    car->addEngine(&engine_mock);
+
+    EXPECT_CALL(engine_mock, getType).Times(1).WillOnce(::testing::Return(BROKEN));
+
+    EXPECT_EQ(car->isEngineBroken(), true);
+}
+
 TEST_F(CarTest, isValidCheckForSuvReturnTrue)
 {
     ICar* car = factory.getCar(SUV);
@@ -117,4 +141,62 @@ TEST_F(CarTest, isValidCheckForSuvReturnTrue)
     EXPECT_CALL(steering_mock, getType).Times(1).WillOnce(testing::Return(BOSCH_S));
 
     EXPECT_EQ(car->isValidCheck(), true);
+}
+
+TEST_F(CarTest, PrintTestForSuv)
+{
+    ICar* car = factory.getCar(SUV);
+    EngineMock engine_mock;
+    BrakeSysteMock brake_mock;
+    SteeringMock steering_mock;
+
+    car->addEngine(&engine_mock);
+    car->addBreakSystem(&brake_mock);
+    car->addSteering(&steering_mock);
+
+    EXPECT_CALL(engine_mock, getName).Times(1).WillOnce(testing::Return("GM"));
+    EXPECT_CALL(brake_mock, getName).Times(1).WillOnce(testing::Return("MANDO"));
+    EXPECT_CALL(steering_mock, getName).Times(1).WillOnce(testing::Return("MOBIS"));
+
+    car->print();
+
+    EXPECT_EQ(1, 1);
+}
+
+TEST_F(CarTest, getNameForTruck)
+{
+    ICar* car = factory.getCar(TRUCK);
+    EXPECT_EQ(car->getName(), "Truck");
+}
+
+TEST_F(CarTest, isEngineBrokenForTruckTrue)
+{
+    ICar* car = factory.getCar(TRUCK);
+    EngineMock engine_mock;
+
+    car->addEngine(&engine_mock);
+
+    EXPECT_CALL(engine_mock, getType).Times(1).WillOnce(::testing::Return(BROKEN));
+
+    EXPECT_EQ(car->isEngineBroken(), true);
+}
+
+TEST_F(CarTest, PrintTestForTruck)
+{
+    ICar* car = factory.getCar(TRUCK);
+    EngineMock engine_mock;
+    BrakeSysteMock brake_mock;
+    SteeringMock steering_mock;
+
+    car->addEngine(&engine_mock);
+    car->addBreakSystem(&brake_mock);
+    car->addSteering(&steering_mock);
+
+    EXPECT_CALL(engine_mock, getName).Times(1).WillOnce(testing::Return("GM"));
+    EXPECT_CALL(brake_mock, getName).Times(1).WillOnce(testing::Return("MANDO"));
+    EXPECT_CALL(steering_mock, getName).Times(1).WillOnce(testing::Return("MOBIS"));
+
+    car->print();
+
+    EXPECT_EQ(1, 1);
 }
