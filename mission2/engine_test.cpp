ec683e4a12b6132a6031101ae25ea062c2918315
AdMelioraDev <sj611.dev@gmail.com>
2025-06-27 17:09:38 +0900

    중간 제출3

--- mission2/engine_test.cpp
+++ mission2/engine_test.cpp
@@ -21,3 +21,45 @@ TEST_F(EngineTest, getNameForGmEngine)
     IEngine* gm_engine = factory.getEngine(GM);
     EXPECT_EQ(gm_engine->getName(), "GM");
 }
+
+TEST_F(EngineTest, getTypeForGmEngine)
+{
+    IEngine* gm_engine = factory.getEngine(GM);
+    EXPECT_EQ(gm_engine->getType(), GM);
+}
+
+TEST_F(EngineTest, getNameForToyota)
+{
+    IEngine* gm_engine = factory.getEngine(TOYOTA);
+    EXPECT_EQ(gm_engine->getName(), "TOYOTA");
+}
+
+TEST_F(EngineTest, getTypeForToyota)
+{
+    IEngine* gm_engine = factory.getEngine(TOYOTA);
+    EXPECT_EQ(gm_engine->getType(), TOYOTA);
+}
+
+TEST_F(EngineTest, getNameForWia)
+{
+    IEngine* gm_engine = factory.getEngine(WIA);
+    EXPECT_EQ(gm_engine->getName(), "WIA");
+}
+
+TEST_F(EngineTest, getTypeForWia)
+{
+    IEngine* gm_engine = factory.getEngine(WIA);
+    EXPECT_EQ(gm_engine->getType(), WIA);
+}
+
+TEST_F(EngineTest, getNameForBroken)
+{
+    IEngine* gm_engine = factory.getEngine(BROKEN);
+    EXPECT_EQ(gm_engine->getName(), "°íÀå³­");
+}
+
+TEST_F(EngineTest, getTypeForBroken)
+{
+    IEngine* gm_engine = factory.getEngine(BROKEN);
+    EXPECT_EQ(gm_engine->getType(), BROKEN);
+}
