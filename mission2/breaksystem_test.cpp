92de665ab899e852380b4d63c692253ab1049f4a
AdMelioraDev <sj611.dev@gmail.com>
2025-06-27 17:14:01 +0900

    중간 제출 4

--- mission2/breaksystem_test.cpp
+++ mission2/breaksystem_test.cpp
@@ -16,8 +16,39 @@ public:
     BreakSystemFactory factory;
 };
 
-TEST_F(BrakeTest, getNameForGmEngine)
+TEST_F(BrakeTest, getNameForMando)
 {
     IBreakSystem* brake = factory.getBreakSystem(MANDO);
     EXPECT_EQ(brake->getName(), "MANDO");
 }
+
+TEST_F(BrakeTest, getTypeForMando)
+{
+    IBreakSystem* brake = factory.getBreakSystem(MANDO);
+    EXPECT_EQ(brake->getType(), MANDO);
+}
+
+TEST_F(BrakeTest, getNameForContinental)
+{
+    IBreakSystem* brake = factory.getBreakSystem(CONTINENTAL);
+    EXPECT_EQ(brake->getName(), "CONTINENTAL");
+}
+
+TEST_F(BrakeTest, getTypeForContinental)
+{
+    IBreakSystem* brake = factory.getBreakSystem(CONTINENTAL);
+    EXPECT_EQ(brake->getType(), CONTINENTAL);
+}
+
+TEST_F(BrakeTest, getNameForBosch)
+{
+    IBreakSystem* brake = factory.getBreakSystem(BOSCH_B);
+    EXPECT_EQ(brake->getName(), "BOSCH");
+}
+
+TEST_F(BrakeTest, getTypeForBosch)
+{
+    IBreakSystem* brake = factory.getBreakSystem(BOSCH_B);
+    EXPECT_EQ(brake->getType(), BOSCH_B);
+}
+
