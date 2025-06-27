015126e6e6e5d68120a844bc5756f771ad6ca435
AdMelioraDev <sj611.dev@gmail.com>
2025-06-27 17:17:20 +0900

    중간 제출 5

--- mission2/steering_test.cpp
+++ mission2/steering_test.cpp
@@ -21,3 +21,21 @@ TEST_F(SteeringTest, getNameForBosch)
     ISteering* bosch = factory.getSteering(BOSCH_S);
     EXPECT_EQ(bosch->getName(), "BOSCH");
 }
+
+TEST_F(SteeringTest, getTypeForBosch)
+{
+    ISteering* bosch = factory.getSteering(BOSCH_S);
+    EXPECT_EQ(bosch->getType(), BOSCH_S);
+}
+
+TEST_F(SteeringTest, getNameForMobis)
+{
+    ISteering* bosch = factory.getSteering(MOBIS);
+    EXPECT_EQ(bosch->getName(), "MOBIS");
+}
+
+TEST_F(SteeringTest, getTypeForMobis)
+{
+    ISteering* bosch = factory.getSteering(MOBIS);
+    EXPECT_EQ(bosch->getType(), MOBIS);
+}
